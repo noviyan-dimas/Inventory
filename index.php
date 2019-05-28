8bc2c5bed7601db204b4a30975406d9fb27e91f6
gigihko <50349603+gigihko@users.noreply.github.com>
2019-05-28 15:13:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
-<!DOCYTE html>
-<html>
-<head>
-	<tittle>Sewa Inventaris | Mahasiswa UNNES</tittle>
-</head>
-<body>
-	<header>
-		<h3>Pendaftaran Member Baru</h3>
-		<h1>Mahasiswa UNNES</h1>
-	</header>
-	
-	<h4>Menu</h4>
-	<nav>
-		<ul>
-			<li><a href="form-daftar.php">Daftar Baru</a></li>
-			<li><a href="list-member.php">Daftar Member</a></li>
-		</ul>
-		<?php if(isset($_GET['status'])): ?>
-	<p>
-		<?php
-			if($_GET['status'] == 'sukses'){
-				echo "Pendaftaran member baru berhasil!";
-			} else {
-				echo "Pendaftaran gagal!";
-			}
-		?>
-	</p>
-		<?php endif; ?>
-	</nav>
-</body>
-</html>
+<?php
+
+
+require __DIR__.'/../bootstrap_2/autoload.php';
+
+
+
+$app = require_once __DIR__.'/../bootstrap_2/start.php';
+
+
+
+$app->run();
